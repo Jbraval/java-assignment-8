bc6c5bff4a03850e98c73955b3241415deb47c12
Jbraval <128069825+Jbraval@users.noreply.github.com>
2023-04-09 18:35:22 +0530

    Add files via upload

---
+++ q4.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class q4 {
+    public static boolean is_leapYear(int y){
+        boolean a =(y%4==0);
+        boolean b=(y%100!=0);
+        boolean c=((y%100==0) &&  (y%400==0));
+        return a && (b||c);
+    }
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter the year : ");
+        int year = sc.nextInt();
+        System.out.println(is_leapYear(year));
+    }
+}
